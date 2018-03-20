534769c61295a5af74cce5c0893325d307135810
ilan2510 <ilan252000@gmail.com>
2018-03-20 15:22:19 +0200

    android 1.6(if there isn't 1.5 so this is 1.5)

---
+++ app/src/main/java/com/example/ailan/example2/AccountAdapter.java
@@ -0,0 +1,48 @@
+package com.example.ailan.example2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by yazamut12 on 20/03/2018.
+ */
+
+public class AccountAdapter extends ArrayAdapter {
+    Context context;
+    List<Account> objects;
+    public AccountAdapter(Context context, int resource, List<Account> objects) {
+        super(context, resource, objects);
+        this.objects=objects;
+        this.context=context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = ((Activity)context).getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.custom_rowsec2, parent, false);
+
+         TextView tvaccFname = (TextView) view.findViewById(R.id.tvaccFname);
+         TextView tvaccLname = (TextView) view.findViewById(R.id.tvaccLname);
+         TextView tvdisplayName = (TextView) view.findViewById(R.id.tvaccDisplayname);
+         TextView tvemail = (TextView) view.findViewById(R.id.tvEmail);
+         TextView tvspecAccId = (TextView) view.findViewById(R.id.tvSpecId);
+         Account temp = objects.get(position);
+         tvaccFname.setText(temp.getFirstName());
+         tvaccLname.setText(temp.getLastName());
+         tvdisplayName.setText(temp.getDisplayName());
+         tvemail.setText(temp.getEmail());
+         tvspecAccId.setText(temp.getAccSpecId());
+
+         return view;
+
+    }
+}
